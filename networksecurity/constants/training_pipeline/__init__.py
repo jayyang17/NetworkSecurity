1ef7e9a3625d56b27f01da5a9a7102d6b6d45480
Jay Yang <jayyanglee17@gmail.com>
2024-12-07 12:54:59 +0800

    Data Validation Completed

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -6,20 +6,17 @@ import pandas as pd
 """
 defining common constant variable for training pipelin
 """
-
 TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phishingData.csv"
-
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str =  "test.csv"
-
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
 
 """
 Data ingestion related constant start with DATA_INGESTION VAR NAME
 """
-
 DATA_INGESTION_COLLECTION_NAME : str = "NetworkData"
 DATA_INGESTION_DATABASE_NAME : str = "JAYYL"
 DATA_INGESTION_DIR_NAME : str = "data_ingestion"
@@ -27,3 +24,11 @@ DATA_INGESTION_FEATURE_STORE_DIR : str = "feature_store"
 DATA_INGESTION_INGESTED_DIR : str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATION : float = 0.2
 
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated" 
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
