1738509a544a419a80bb721d719b6fd4a7982d5d
Jay Yang <jayyanglee17@gmail.com>
2024-12-08 11:17:55 +0800

    ml trainer evaluation and mlflow commited

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import (DataIngestionConfig, 
                                                   DataValidationConfig, 
-                                                  DataTransformationConfig
+                                                  DataTransformationConfig,
+                                                  ModelTrainerConfig
 )
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
@@ -41,5 +43,12 @@ if __name__=="__main__":
         logging.info("Data Transformation Completed")
         print(data_transformation_artifact)
         
+        # Model Trainer Step
+        logging.info("Model Training started")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,
+                                   data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
     except Exception as e:
         raise NetworkSecurityException(e, sys)
---
+++ networksecurity/utils/ml_utils/model/estimator.py
@@ -0,0 +1,20 @@
+import os, sys
+from networksecurity.entity.artifact_entity import ClassificationMetricArtifact
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.constants.training_pipeline import SAVED_MODEL_DIR,MODEL_FILE_NAME
+
+class NetworkModel:
+    def __init__(self,preprocessor,model):
+        try:
+            self.preprocessor = preprocessor
+            self.model = model
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def predict(self, x):
+        try:
+            x_transform = self.preprocessor.transform(x)
+            y_hat = self.model.predict(x_transform)
+            return y_hat
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
